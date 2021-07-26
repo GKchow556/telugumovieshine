0973dd41d251e058a2d61c2131af500233b8f95c
GKchow556 <koti.gannama@gmail.com>
2021-07-26 16:08:08 +0530

    Add files via upload

---
+++ docs/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="description" content="Telugu News, Telugu Cinema News, Telugu Movie News, Telugu Film News, Tollywood News, Tollywood Latest News, Latest Tollywood News, Telugu Movie Reviews, Telugu Cinema Reviews, Tollywood Movie Reviews, Tollywood Film Reviews, Andhra News, Telangana News, Telugu Cinema Videos, Andhra Political News, Telugu Cinema Actress Photos, Hot Gossips, Tollywood Gossips and Tv Shows"
+    />
+    <meta name="keywords" content="Latest Telugu News, Breaking News In Telugu, Online Telugu News, Today Telugu News, News In Telugu, Telugu Headlines, Telugu News Live, headlines in telugu, Andhra Pradesh Political News, Telugu News, National Political News in Telugu, Actress Galleries , తెలుగు న్యూస్, Tupaki"
+    />
+    <?php include ('include/head.html'); ?>
+</head>
+
+<body class="sticky-header" onload="display_c();">
+    <div class="body-innerwrapper">
+        <header>
+            <?php include ('include/header.html') ?>
+        </header>
+        <section class="main-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-9">
+                        <?php include('include/home-content.html') ?>
+                    </div>
+                    <div class="col-sm-3">
+                        <?php include('include/asside.html') ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer>
+            <?php include('include/footer.html') ?>
+        </footer>
+        <?php include('include/mobile-menu.html') ?>
+    </div>
+    <?php include('include/scripts.html') ?>
+</body>
+</html>
